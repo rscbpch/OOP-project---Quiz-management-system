b00a566f23aacc9cb12d90f85d9a80a06d146899
praseth002 <prasetsokha@gmail.com>
2025-02-11 08:34:06 +0700

    yea idk

--- Project/src/class_structures/Qcm.java
+++ Project/src/class_structures/Qcm.java
@@ -1,11 +1,11 @@
 package class_structures;
 
-public abstract class Qcm {
-    protected int id;
-    protected int quizId;
-    protected String question;
-    private String[] options; 
-    private String correctAnswer;
+public class Qcm {
+    public int id;
+    public int quizId;
+    public String question;
+    private String[] options; // 4 options
+    private String correctAnswer;// change maybe to index of options
 
     public Qcm(int id, int quizId, String question, String[] options, String correctAnswer) {
         this.id = id;
@@ -13,5 +13,15 @@ public abstract class Qcm {
         this.question = question;
         this.options = options;
         this.correctAnswer = correctAnswer;
+    }
+
+    public static void main(String[] args) {
+        Qcm q1 = new Qcm(1, 1, "what is life", new String[]{"a.joe mama","b.jeff"}, "a");
+        System.out.println("this is one qs: ");
+        System.out.println(q1.id);
+        System.out.println(q1.quizId);
+        System.out.println(q1.question);
+        System.out.println(q1.options[0] + " " + q1.options[1]);
+        System.out.println(q1.correctAnswer);
     }
 }
