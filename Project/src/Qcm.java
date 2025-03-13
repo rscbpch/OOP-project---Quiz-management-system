964813a23365469862406a6ae61dda5db86202c6
praseth002 <prasetsokha@gmail.com>
2025-03-13 21:13:27 +0700

    first commit from seth since 1987

--- Project/src/Qcm.java
+++ Project/src/Qcm.java
@@ -3,9 +3,10 @@ public class Qcm {
     public int quizId;
     public String question;
     private String[] options; // 4 options
-    private String correctAnswer;// change maybe to index of options
+    private int correctAnswer;// change maybe to index of options
 
-    public Qcm(int id, int quizId, String question, String[] options, String correctAnswer) {
+    //constructor
+    public Qcm(int id, int quizId, String question, String[] options, int correctAnswer) {
         this.id = id;
         this.quizId = quizId;
         this.question = question;
@@ -13,17 +14,10 @@ public class Qcm {
         this.correctAnswer = correctAnswer;
     }
 
-    public static void main(String[] args) {
-        Qcm q1 = new Qcm(1, 1, "what is life", new String[]{"a.joe mama","b.jeff"}, "a");
-        System.out.println("this is one qs: ");
-        System.out.println(q1.id);
-        System.out.println(q1.quizId);
-        System.out.println(q1.question);
-        System.out.println(q1.options[0] + " " + q1.options[1]);
-        System.out.println(q1.correctAnswer);
-    }
+    //getter methods
+    public int getQuizId () { return quizId;}
+    public String getQuestion () {return question;}
+    public String[] getOptions () {return options;}
+    public int getCorrectAnswer () {return correctAnswer;}
 
-    public String getCorrectAnswer() {
-        return correctAnswer;
-    }
 }
--- Project/src/Quiz.java
+++ Project/src/Quiz.java
@@ -1,31 +1,27 @@
+import java.util.*;
+
 public class Quiz {
     private int id;
     private String title;
     private int creator; 
+    private ArrayList<Qcm> questions; //array to store the questions from qcm
+    //used arraylist cuz its dynamic(?) idk bruh
 
     public Quiz(int id, String title, int creator) {
         this.id = id;
         this.title = title;
         this.creator = creator;
+        this.questions = new ArrayList<>();
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() {return id;}
+    public String getTitle() {return title;}
+    public int getCreator() {return creator;}
+    public void setTitle(String title) {this.title = title;}
+    public void setCreator(int creator) {this.creator = creator;}
 
-    public String getTitle() {
-        return title;
-    }
-
-    public int getCreator() {
-        return creator;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setCreator(int creator) {
-        this.creator = creator;
+    //method to add said questions from qcm to the arraylist
+    public void addQuestion(Qcm question) {
+        questions.add(question);
     }
 }
